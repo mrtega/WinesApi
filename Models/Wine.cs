08e0468ad801f19edb71c7bc11cfad5a0d9a20d3
mrtega <tegaodiete@gmail.com>
2020-06-24 02:52:55 +0100

    Add project files.

---
+++ Models/Wine.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WinesApi.Models
+{
+    public class Wine
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string WineName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Country { get; set; }
+    }
+}
---
+++ Models/WineCellarDatabaseSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WinesApi.Models
+{
+    public class WineCellarDatabaseSettings : IWineCellarDatabaseSettings
+    {
+        public string WinesCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IWineCellarDatabaseSettings
+    {
+        string WinesCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
